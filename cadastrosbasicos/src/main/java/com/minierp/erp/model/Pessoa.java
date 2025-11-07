bdbef2ae8f17d1723421c19081cf698eff71cfae
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-11-07 19:24:36 -0300

    criacao do frontend formulario de cadastro pessoas

--- cadastrosbasicos/src/main/java/com/minierp/erp/model/Pessoa.java
+++ cadastrosbasicos/src/main/java/com/minierp/erp/model/Pessoa.java
@@ -1,27 +1,29 @@
 package com.minierp.erp.model;
 
-import javax.persistence.*;
-import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "pessoa")
-public class Pessoa implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class Pessoa {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 100, nullable = false)
     private String nome;
-
-    @Column(length = 100, nullable = false)
-    private String sobrenome;
+    private String email;
+    private Integer idade;
+    private String sexo;
 
     // Getters e Setters
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -29,14 +31,32 @@ public class Pessoa implements Serializable {
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getSobrenome() {
-        return sobrenome;
+    public String getEmail() {
+        return email;
     }
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
     }
 }
---
+++ cadastrosbasicos/src/main/java/com/minierp/erp/repository/SchemaGeneration.java
@@ -0,0 +1,5 @@
+package com.minierp.erp.repository;
+
+public class SchemaGeneration {
+
+}
