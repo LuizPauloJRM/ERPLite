d2b4f6c9386ed54cf465cdaab47e1aa972ed2f02
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-11-07 19:04:49 -0300

    persistence dao e entidade e primeira pagina jsf

---
+++ cadastrosbasicos/src/main/java/com/minierp/erp/model/Pessoa.java
@@ -0,0 +1,42 @@
+package com.minierp.erp.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String nome;
+
+    @Column(length = 100, nullable = false)
+    private String sobrenome;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+}
